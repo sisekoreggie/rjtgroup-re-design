0578888f2924a7c40dc2ca836ca31a6ab3e60303
sisekoreggie <3443307@myuwc.ac.za>
2020-05-29 17:32:47 +0200

    static react.js site

---
+++ precache-manifest.9834bc8aca7db266d42447fc0b450aac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d129d5ed98d7772b4f615fefb3265abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e63f1ed791d535b7233",
+    "url": "/static/css/2.674f0730.chunk.css"
+  },
+  {
+    "revision": "d195eefc1272cd46c526",
+    "url": "/static/css/main.ddc21023.chunk.css"
+  },
+  {
+    "revision": "5e63f1ed791d535b7233",
+    "url": "/static/js/2.d3d5ba21.chunk.js"
+  },
+  {
+    "revision": "cb7d29aba36de65f7d2a303a6e593c7f",
+    "url": "/static/js/2.d3d5ba21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d195eefc1272cd46c526",
+    "url": "/static/js/main.a647bc65.chunk.js"
+  },
+  {
+    "revision": "2dd2c5ff042b8be148f3",
+    "url": "/static/js/runtime-main.14d5cd0a.js"
+  },
+  {
+    "revision": "919babb1a95dacb1a680ad567c41a215",
+    "url": "/static/media/Logo.919babb1.png"
+  },
+  {
+    "revision": "0453485e7398b7bce32e3bd15e96564f",
+    "url": "/static/media/about-us.0453485e.png"
+  },
+  {
+    "revision": "f500af7efefce23fc0baa82f29c5d1dc",
+    "url": "/static/media/background-image.f500af7e.jpg"
+  },
+  {
+    "revision": "2d992818223ecf251017d0acda9b00cd",
+    "url": "/static/media/diesel-truck-1.2d992818.jpeg"
+  },
+  {
+    "revision": "5ff133d69e948c22bcfac20dba43e20b",
+    "url": "/static/media/diesel-truck-2.5ff133d6.jpeg"
+  },
+  {
+    "revision": "6ce88690ffa6bb1122d88e69e7339e97",
+    "url": "/static/media/diesel-truck-3.6ce88690.jpeg"
+  }
+]);
